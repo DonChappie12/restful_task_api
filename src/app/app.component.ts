96dd80b8e5701dc18f4b444c261021837bac8e5f
David <oneproductions12@gmail.com>
2018-07-16 15:17:40 -0700

    push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpService } from './http.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Mean';
+  allTasks: any;
+  oneTask: any;
+  constructor(private _httpService: HttpService){
+    // this.getAllTasks();
+    // this.getOneTask('');
+  }
+  getAllTasks(){
+    let x = this._httpService.getTasks()
+    x.subscribe(data=>{this.allTasks = data;
+    console.log(this.allTasks)})
+  }
+  getOneTask(id){
+    let z = this._httpService.getOneTask(id)
+    z.subscribe(data=>{
+      this.oneTask = data;
+      // console.log(this.oneTasks) chmodtriple7
+    })
+  }
+  // onButtonClick(): void { 
+  //   console.log(`Click event is working`);
+  // }
+  // onButtonClickParam(num: Number): void { 
+  //   console.log(`Click event is working with num param: ${num}`);
+  // }
+  // onButtonClickParams(num: Number, str: String): void { 
+  //   console.log(`Click event is working with num param: ${num} and str param: ${str}`);
+  // }
+  // onButtonClickEvent(event: any): void { 
+  //   console.log(`Click event is working with event: ${event}`);
+  // }
 }
---
+++ src/app/http.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+  constructor(private _http: HttpClient) {
+      // this.getTasks();
+      // this.getOneTask();
+  }
+  getTasks(){
+    return this._http.get('/api/tasks');
+  }
+  getOneTask(id){
+    return this._http.get('/api/tasks/'+id);
+  }
+}
